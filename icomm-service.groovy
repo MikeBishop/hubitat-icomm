bf66aaf1d8fb5a51b05485b4ac067d65748c9c87
Mike Bishop <mbishop@evequefou.be>
2025-05-09 21:21:53 -0400

    Initial commit

---
+++ icomm-service.groovy
@@ -0,0 +1,380 @@
+/*
+* Hubitat-iCONN Integration
+* Copyright 2025 Mike Bishop,  All Rights Reserved
+*
+* Based on:
+* - py-aosmith by bdr99, https://github.com/bdr99/py-aosmith/
+* - Schluter Ditra driver by Marc Reyhner, https://github.com/marcre/hubitat-drivers
+*
+* This Hubitat driver is designed for use with a State/A.O. Smith iCOMM-enabled
+* water heater.
+*
+* Preferences:
+* Email Address = REQUIRED. The e-mail address for the iCOMM account
+* Password = REQUIRED. Login password for the iCOMM account
+* RefreshRate = REQUIRED; DEFAULT is 5 minutes. The rate at which the water heaters will be polled
+* DebugLogs = OPTIONAL - DEFAULT = false. Should debug logging be enabled?
+*/
+
+import java.time.format.DateTimeFormatter
+import java.time.OffsetDateTime
+import groovy.transform.Field
+import java.time.LocalDate
+
+@Field static final String AOSMITH = "aosmith"
+@Field static final String STATE = "state"
+@Field static final String BASE_URI = "https://r2.wh8.co"
+@Field static final String APP_VERSION = "13.0.2"
+@Field static final String USER_AGENT = "okhttp/4.9.2"
+
+metadata{
+    definition ( name: "iCONN", namespace: "evequefou", author: "Mike Bishop", importUrl: "https://github.com/MikeBishop/hubitat-icomm/icomm-service.groovy" ) {
+        // Attempting to indicate what capabilities the device should be capable of
+        capability "Refresh"
+
+        // Keep track of connection status
+        attribute "Status", "string"
+    }
+    preferences{
+        section{
+            // Login information for the iCOMM account
+            input( type: "string", name: "EmailAddress", title: "<font color='FF0000'><b>iCONN account e-mail address</b></font>", required: true )
+            input( type: "password", name: "Password", title: "<font color='FF0000'><b>iCONN account password</b></font>", required: true )
+
+            input( type: "enum", name: "Brand", title: "<b>Brand</b>", required: true, options: [ (AOSMITH): "A.O. Smith", (STATE): "State" ], defaultValue: "A.O. Smith" )
+
+            // Enum to allow selecting the refresh rate that the device will be checked
+            input( type: "enum", name: "RefreshRate", title: "<b>Refresh Rate</b>", required: false, multiple: false, options: [ "15 seconds", "30 seconds", "1 minute", "5 minutes", "10 minutes", "15 minutes", "30 minutes", "1 hour", "3 hours", "Manual" ], defaultValue: "5 minutes" )
+
+            // Enum to set the level of logging that will be used
+            input( type: "bool", name: "DebugLogs", title: "<b>Enable Debug Logging?</b>", required: true, multiple: false, defaultValue: false )
+        }
+    }
+}
+
+// uninstalling device so make sure to clean up children
+void uninstalled() {
+	// Delete all children
+	getChildDevices().each{
+		deleteChildDevice( it.deviceNetworkId )
+	}
+    log.info("Uninstalled")
+}
+
+// updated is called whenever device parameters are saved
+// Sets the current version of the driver, basic settings, and schedules
+def updated() {
+
+    // On update clear out any old session id in case login information changed
+    state.remove("accessToken")
+
+    // Check if the refresh rate is not set for some reason and putting it at the default
+    if( RefreshRate == null ) {
+        RefreshRate = "5 minutes"
+    }
+
+    def Hour = ( new Date().format( "h" ) as int )
+    def Minute = ( new Date().format( "m" ) as int )
+    def Second = ( new Date().format( "s" ) as int )
+    Second = ( (Second + 5) % 60 )
+
+    // Check what the refresh rate is set for then run it
+    switch( RefreshRate ){
+        case "15 seconds": // Schedule the refresh check for every 15 seconds
+            schedule( "0/15 * * ? * *", "refresh" )
+            break
+        case "30 seconds": // Schedule the refresh check for every 30 seconds
+            schedule( "0/30 * * ? * *", "refresh" )
+            break
+        case "1 minute": // Schedule the refresh check for every minute
+            schedule( "${ Second } * * ? * *", "refresh" )
+            break
+        case "5 minutes": // Schedule the refresh check for every 5 minutes
+            schedule( "${ Second } 0/5 * ? * *", "refresh" )
+            break
+        case "10 minutes": // Schedule the refresh check for every 10 minutes
+            schedule( "${ Second } 0/10 * ? * *", "refresh" )
+            break
+        case "15 minutes": // Schedule the refresh check for every 15 minutes
+            schedule( "${ Second } 0/15 * ? * *", "refresh" )
+            break
+        case "30 minutes": // Schedule the refresh check for every 30 minutes
+            schedule( "${ Second } 0/30 * ? * *", "refresh" )
+            break
+        case "1 hour": // Schedule the refresh check for every hour
+            schedule( "${ Second } ${ Minute } * ? * *", "refresh" )
+            break
+        case "3 hours": // Schedule the refresh check for every 3 hours
+            schedule( "${ Second } ${ Minute } 0/3 ? * *", "refresh" )
+            break
+        default:
+            unschedule( "refresh" )
+            RefreshRate = "Manual"
+            break
+    }
+    log.info( "Refresh rate: ${ RefreshRate }" )
+
+    log.info("Updated")
+}
+
+def CanProceed() {
+    if( EmailAddress == null || Password == null){
+        UpsertAttribute( "Status", "Missing account email or password" )
+        log.error( "Cannot update without username and password" )
+
+        return false
+    }
+
+    return true
+}
+
+// refresh runs the device polling
+def refresh() {
+
+    if (!CanProceed()) {
+        return
+    }
+
+    try {
+
+        if( LoginIfNoToken() ) {
+            // Wait for the login to complete
+            runIn(5, "refresh")
+        }
+        else {
+            sendGraphQLRequest(GET_DEVICES_QUERY, [:], "ProcessGetDevicesResponse")
+        }
+
+    }
+    catch (IOException e)
+    {
+        log.error( "Error connecting to API for status. ${e}" )
+        UpsertAttribute( "Status", "Local Connection Failed: ${e.message}" )
+    }
+}
+
+def LoginIfNoToken() {
+
+    if (state.containsKey("accessToken")) {
+        if (DebugLogsEnabled()) log.trace("Valid session, no need to request new")
+
+        return false
+    }
+
+    def credentials = ['email': EmailAddress, 'password': Password];
+    def jsonString = groovy.json.JsonOutput.toJson(credentials);
+    def urlEncoded = java.net.URLEncoder.encode(jsonString, "UTF-8");
+    def passcode = new String(
+        org.apache.commons.codec.binary.Base64.encodeBase64(urlEncoded.getBytes("UTF-8")),
+        "UTF-8"
+    );
+
+    sendGraphQLRequest(LOGIN_QUERY, [passcode: passcode], "ProcessLoginResponse", false)
+    return true
+}
+
+def ProcessLoginResponse(response) {
+    if (DebugLogsEnabled()) {
+        log.debug("Login raw data = \"${response.getData()}\"")
+    }
+
+    state.accessToken = response.getData()?.data?.login?.user?.tokens?.accessToken
+
+    if (state.accessToken == null) {
+        log.error("Login failed, no access token received.")
+        UpsertAttribute( "Status", "Login failed" )
+        return
+    }
+    else if (DebugLogsEnabled()) {
+        log.debug("Succesfully logged in to iCOMM API.")
+    }
+}
+
+def ProcessGetDevicesResponse(response) {
+    if( DebugLogsEnabled() ) {
+        log.debug("Got response ${response.getData()?.data?.devices}")
+    }
+
+    def waterHeaters = response.getData()?.data?.devices.findAll {
+        ["NextGenHeatPump", "RE3Connected"].contains(it?.data?.__typename)
+    };
+
+    for (heater in waterHeaters) {
+        ProcessDeviceUpdate(heater)
+    }
+
+    getChildDevices().each{ cd ->
+        if (!waterHeaters.any { it.junctionId == cd.deviceNetworkId }) {
+            log.info("Removing child device ${cd.deviceNetworkId}, ${cd.name}.")
+            deleteChildDevice( cd.deviceNetworkId );
+        }
+    }
+}
+
+def ProcessDeviceUpdate(heater) {
+
+    if (!getChildDevice(heater.junctionId)) {
+        addChildDevice("iCONNWaterHeater", heater.junctionId, [
+            isComponent: true,
+            name: heater.name ?: "${heater.install.location} Water Heater",
+        ])
+    }
+
+    getChildDevice(heater.junctionId).ProcessUpdate(heater)
+}
+
+def ProcessModeChange(response) {
+    ProcessChange(response, "updateMode");
+}
+
+def ProcessSetpointChange(response) {
+    ProcessChange(response, "updateSetpoint")
+}
+
+def ProcessChange(response, fieldToCheck) {
+    if (DebugLogsEnabled()) {
+        log.debug("State change response: ${response.getData()}")
+    }
+
+    def data = response.getData()?.data;
+    if (!data || data[fieldToCheck] != true) {
+        log.error("Failed to change mode: ${response.getData()}")
+    }
+
+    refresh()
+}
+
+def sendGraphQLRequest(query, variables, handler, autologin = true) {
+    def headers = [
+        "brand": Brand,
+        "version": APP_VERSION,
+        "User-Agent": USER_AGENT,
+    ];
+
+    if( state.accessToken != null ) {
+        headers["Authorization"] = "Bearer ${state.accessToken}"
+    }
+
+    def body = [
+        "query": query,
+        "variables": variables
+    ]
+
+    def uri = "${BASE_URI}/graphql"
+
+    httpPostJson([
+        uri: uri,
+        headers: headers,
+        body: body
+    ]) { response ->
+        def status = response.getStatus();
+        def data = response.getData();
+
+        if (
+            (status == 401 || data?.errors?.extensions.any{ it.code == "UNAUTHORIZED_ERROR" }) &&
+            autologin
+        ) {
+            if (DebugLogsEnabled()) log.debug("Session expired, logging in again")
+
+            state.remove("accessToken")
+            LoginIfNoToken()
+
+            // Retry the request after logging in
+            runIn(5, "sendGraphQLRequest", [data: [query: query, variables: variables, handler: handler, autologin: false]])
+        } else if (status == 200) {
+            if (DebugLogsEnabled()) log.debug("GraphQL request succeeded: ${response.getData()}")
+
+            this."$handler"(response)
+        } else {
+            log.error("GraphQL request failed with status ${status}: ${response.getData()}")
+        }
+    }
+
+}
+
+def sendGraphQLRequest(Map params) {
+    def query = params.query
+    def variables = params.variables
+    def handler = params.handler
+    def autologin = params.autologin ?: true
+
+    sendGraphQLRequest(query, variables, handler, autologin)
+}
+
+
+// Update an attribute if it has changed
+def UpsertAttribute( name, value, unit = null ) {
+
+    if (device.currentValue(name) != value) {
+        if (unit != null) {
+            log.info("Event: ${name} = ${value}${unit}")
+            sendEvent(name: name, value: value, unit: unit)
+        } else {
+            log.info("Event: ${name} = ${value}")
+            sendEvent(name: name, value: value)
+        }
+    }
+}
+
+def DebugLogsEnabled() {
+     return DebugLogs
+}
+
+@Field static final String LOGIN_QUERY = """
+query login(\$passcode: String)
+{
+    login(passcode: \$passcode)
+    {
+        user
+        {
+            tokens
+            {
+                accessToken
+                idToken
+                refreshToken
+            }
+        }
+    }
+}
+"""
+
+@Field static final String GET_DEVICES_QUERY = """
+query devices(\$forceUpdate: Boolean, \$junctionIds: [String]) {
+    devices(forceUpdate: \$forceUpdate, junctionIds: \$junctionIds) {
+        brand
+        model
+        deviceType
+        dsn
+        junctionId
+        name
+        serial
+        install {
+            location
+        }
+        data {
+            __typename
+            temperatureSetpoint
+            temperatureSetpointPending
+            temperatureSetpointPrevious
+            temperatureSetpointMaximum
+            modes {
+                mode
+                controls
+            }
+            isOnline
+            ... on NextGenHeatPump {
+                firmwareVersion
+                hotWaterStatus
+                mode
+                modePending
+            }
+            ... on RE3Connected {
+                firmwareVersion
+                hotWaterStatus
+                mode
+                modePending
+            }
+        }
+    }
+}
+"""
---
+++ icomm-water-heater.groovy
@@ -0,0 +1,232 @@
+/*
+* iCOMM Water Heater
+*
+* Description:
+* This Hubitat driver is designed for use with an iCOMM Water Heater.  This is
+* the child device per water heater.  It takes no direct configuration
+*
+* Licensing:
+* Copyright 2025 Mike Bishop
+*
+* Based on:
+* - py-aosmith by bdr99, https://github.com/bdr99/py-aosmith/
+* - Schluter Ditra driver by Marc Reyhner, https://github.com/marcre/hubitat-drivers
+*/
+
+import groovy.transform.Field
+
+metadata{
+    definition ( name: "iCONNWaterHeater", namespace: "evequefou", author: "Mike Bishop", importUrl: "https://github.com/MikeBishop/hubitat-icomm/icomm-water-heater.groovy" ) {
+        capability "Sensor"
+        capability "Refresh"
+        capability "ThermostatHeatingSetpoint"
+        capability "ThermostatOperatingState"
+        capability "ThermostatSetpoint"
+        capability "SwitchLevel"
+        attribute "Brand", "string"
+        attribute "Model", "string"
+        attribute "Device Type", "string"
+        attribute "DSN", "string"
+        attribute "Serial Number", "string"
+        attribute "Install Location", "string"
+        attribute "Pending Temperature", "number"
+        attribute "Maximum Temperature", "number"
+        attribute "Previous Temperature", "number"
+        attribute "Schedule Mode", "string"
+        attribute "Online", "string"
+        attribute "Firmware Version", "string"
+        attribute "Mode", "string"
+        attribute "Pending Mode", "string"
+
+        command "setMode", [
+            [
+                name: "Mode",
+                type: "ENUM",
+                options: [
+                    "HYBRID": "Hybrid",
+                    "HEAT_PUMP": "Heat Pump Only",
+                    "ELECTRIC": "Electric Only",
+                    "VACATION": "Vacation",
+                ]
+            ],
+            [
+                name: "Number of days",
+                type: "NUMBER",
+                required: false,
+                description: "Optional, not supported by all modes"
+            ]
+        ]
+        command "setHeatingSetpoint", [
+            [
+                name: "Temperature",
+                type: "NUMBER",
+                required: true,
+                description: "Temperature in ${location.temperatureScale}"
+            ]
+        ]
+    }
+}
+
+def installed() {
+    // Set static attributes at install time
+    // Currently don't expose any static attributes; might later
+}
+
+def ProcessUpdate(heater) {
+    if (getParent().DebugLogsEnabled()) log.debug("Thermostat raw data ${heater.toString()}")
+
+    UpsertAttribute("Brand", heater.brand)
+    UpsertAttribute("Model", heater.model)
+    UpsertAttribute("Device Type", heater.deviceType)
+    UpsertAttribute("DSN", heater.dsn)
+    device.setName(heater.name)
+    UpsertAttribute("Serial Number", heater.serial)
+    UpsertAttribute("Install Location", heater?.install?.location)
+    UpsertAttribute("Online", heater.online)
+
+    def setpoint = heater?.data?.temperatureSetpoint
+    UpsertAttribute("thermostatSetpoint", setpoint, location.temperatureScale)
+    UpsertAttribute("heatingSetpoint", setpoint, location.temperatureScale)
+    UpsertAttribute(
+        "Pending Temperature",
+        heater?.data?.temperatureSetpointPending
+    );
+    UpsertAttribute(
+        "Maximum Temperature",
+        heater?.data?.temperatureSetpointMaximum,
+        location.temperatureScale
+    );
+    UpsertAttribute(
+        "Previous Temperature",
+        heater?.data?.temperatureSetpointPrevious,
+        location.temperatureScale
+    );
+    UpsertAttribute("Mode", heater?.data?.mode);
+    UpsertAttribute("Pending Mode", heater?.data?.modePending);
+    UpsertAttribute("Online", heater?.data?.isOnline);
+    UpsertAttribute("Firmware Version", heater?.data?.firmwareVersion);
+
+    def waterLevel = 100;
+    def waterLevelVal = heater?.data?.hotWaterStatus;
+    if( waterLevelVal instanceof String ){
+        switch(waterLevelVal){
+            case "LOW":
+                waterLevel = 0;
+                break;
+            case "MEDIUM":
+                waterLevel = 50;
+                break;
+            case "HIGH":
+                waterLevel = 100;
+                break;
+        }
+    }
+    else if ( waterLevelVal instanceof Number ){
+        // Reported number goes up as water is used; need to convert
+        waterLevel = 100 - waterLevelVal;
+    }
+    UpsertAttribute("level", waterLevel, "%")
+
+    state.supportedModes = heater?.data?.modes
+}
+
+def setMode(mode, days = null) {
+    log.info("setMode(${mode}) on ${device.getDisplayName()} invoked.")
+
+    def targetMode = state.supportedModes.find { it.mode == mode }
+
+    if (targetMode == null) {
+        log.warn("setMode(${mode}) on ${device.getDisplayName()} is not supported.")
+        return
+    }
+
+    if (targetMode.controls == "SELECT_DAYS") {
+        // Number of days is required
+        if( days == null ) {
+            days = 100
+        }
+        else if ( days < 1 || days > 100 ) {
+            log.warn("Mode ${mode} on ${device.getDisplayName()} only supports 1-100 days.")
+            return
+        }
+    }
+    else {
+        // Number of days is not supported
+        if( days != null ) {
+            log.warn("Mode ${mode} on ${device.getDisplayName()} does not support setting the number of days.")
+            days = null
+        }
+    }
+
+    def modePayload = ["mode": targetMode.mode];
+    if (days != null) {
+        modePayload["days"] = days
+    }
+
+    getParent().sendGraphQLRequest(SET_MODE, [
+        "junctionId": device.deviceNetworkId,
+        "mode": modePayload
+    ], "ProcessModeChange")
+}
+
+def refresh() {
+    log.info("refresh() on ${device.getDisplayName()} invoked.")
+    getParent().refresh()
+}
+
+def normalizeTemperature(temperature) {
+    minimumTemperature = 95
+
+    if (temperature == null) {
+        return minimumTemperature;
+    }
+
+    if (temperature < minimumTemperature) {
+        log.warn("${temperature} on ${device.getDisplayName()} is less than minimum temperature ${minimumTemperature}.  Will set to minimum temperature.")
+
+        return minimumTemperature;
+    }
+
+    maximumTemperature = device.currentValue("Maximum Temperature")
+
+    if (temperature > maximumTemperature) {
+        log.warn("setHeatingSetpoint(${temperature}) on ${device.getDisplayName()} is greater than maximum temperature ${maximumTemperature}.  Will set to maximum temperature.")
+        log.info("You may be able to increase the maximum temperature from the water heater's control panel.")
+
+        return maximumTemperature;
+    }
+
+    return temperature;
+}
+
+def setHeatingSetpoint(temperature) {
+    log.info("setHeatingSetpoint(${temperature}) on ${device.getDisplayName()} invoked.")
+
+    temperature = normalizeTemperature(temperature)
+
+    getParent().sendGraphQLRequest(
+        SET_HEATING_SETPOINT,
+        [
+            "junctionId": device.deviceNetworkId,
+            "value": temperature
+        ],
+        "ProcessSetpointChange"
+    )
+}
+
+
+def UpsertAttribute( Variable, Value, Unit = null ){
+    if( device.currentValue(Variable) != Value ){
+
+        if( Unit != null ){
+            log.info( "Event: ${ Variable } = ${ Value }${ Unit }" )
+            sendEvent( name: "${ Variable }", value: Value, unit: Unit )
+        } else {
+            log.info( "Event: ${ Variable } = ${ Value }" )
+            sendEvent( name: "${ Variable }", value: Value )
+        }
+    }
+}
+
+@Field static final String SET_MODE = "mutation updateMode(\$junctionId: String!, \$mode: ModeInput!) { updateMode(junctionId: \$junctionId, mode: \$mode) }";
+@Field static final String SET_HEATING_SETPOINT = "mutation updateSetpoint(\$junctionId: String!, \$value: Int!) { updateSetpoint(junctionId: \$junctionId, value: \$value) }";
